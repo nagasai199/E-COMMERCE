1e13b4beca6f93b404b7091e98705818b455dbc4
Nagasai <lknagasai@gmail>
2024-12-16 15:50:53 +0530

    new one

---
+++ index.py
@@ -0,0 +1,5 @@
+# Simple Python program to display "Hello, World!"
+print("Hello, World!")
+# THis is nav
+# Products
+print("This is fotter")
---
+++ login.py
@@ -0,0 +1,9 @@
+# Simple Python program to display "Hello, World!"
+print("Hello, World!")
+# THis is nav
+# Products
+print("This is fotter")
+# To know the changes
+print ("This is Header")
+# for testing perpose
+print("This is login page")
